8173cc5ffd996b14db74609a1b71f55900d421fa
Muhammad Waleed Hassan <waleedhassansfd@gmail.com>
2025-09-29 19:46:27 +0500

    Fix babel config and package versions

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
-module.exports = {
-     presets: ['module:metro-react-native-babel-preset'],
-     plugins: ['react-native-reanimated/plugin'], // Must be last
-   };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin', // 👈 must be last
+    ],
+  };
+};
